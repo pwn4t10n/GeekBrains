0722c18d16914e5d4524eaf0125f28bceff12af7
pwn4t10n <99754508+pwn4t10n@users.noreply.github.com>
2022-08-08 17:49:45 +0300

    Delete Дз1 directory

--- Дз1/num2.py
+++
@@ -1,6 +0,0 @@
-print('Введите количество секунд: ')
-s = int(input())
-hrs = s // 3600
-mnt = (s - hrs * 3600) // 60
-sec = s - hrs * 3600 - mnt * 60
-print(f"{hrs}ч:{mnt}м:{sec}с")
--- Дз1/num4.py
+++
@@ -1,9 +0,0 @@
-print('Введите число: ')
-a = int(input())
-dig = 0
-while a > 10:
-    dv = a % 10
-    a //= 10
-    if dv > dig:
-        dig = dv
-print(f"Самое большое число: {dig}")
