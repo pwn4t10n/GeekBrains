299c031ac7c7fb374fff5d4ee3d852a98591208f
pwn4t10n <99754508+pwn4t10n@users.noreply.github.com>
2022-08-28 10:49:50 +0300

    Переделал третье дз

---
+++ 3.5.2.py
@@ -0,0 +1,19 @@
+def digsum():
+    r = 0
+    ex = False
+    while not ex:
+        number = input('Введите число или Q для выхода: ').split()
+
+        res = 0
+        for el in range(len(number)):
+            if number[el] == 'q' or number[el] == 'Q':
+                ex = True
+                break
+            else:
+                res = res + int(number[el])
+        r = r + res
+        print(f'Нынешняя сумма — {r}')
+    print(f'Конечный результат — {r}')
+
+
+digsum()
